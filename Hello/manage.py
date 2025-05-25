7d4c647f0b2fe66d083955938d783b56d7946c2d
Hemanth kumar gogineni <97873774+Kumargogi@users.noreply.github.com>
2025-05-25 19:08:59 +0530

    Update manage.py

--- Hello/manage.py
+++ Hello/manage.py
@@ -16,8 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    print("end end")
-
+    
 
 if __name__ == '__main__':
     main()
